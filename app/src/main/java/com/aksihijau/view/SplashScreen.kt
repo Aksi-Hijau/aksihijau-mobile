8bc36018ed7a0c973a6d8d976f664255c5e5a41f
REIN <reypow@student.telkomuniversity.ac.id>
2023-05-24 03:14:29 +0700

    Menambahkan Splash screen

--- app/src/main/java/com/aksihijau/view/SplashScreen.kt
+++ app/src/main/java/com/aksihijau/view/SplashScreen.kt
@@ -1,12 +1,20 @@
 package com.aksihijau.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.aksihijau.MainActivity
 import com.aksihijau.R
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+            finish()
+        }, 3000)
     }
 }
